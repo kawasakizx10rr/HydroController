bffd9caf5c5345d017ee11c5f841242d0f3b5c3d
Edwin Martin <husqvarna1990@yahoo.com>
2024-02-17 17:52:32 +0000

    Add files via upload

---
+++ Code/Beta_test/generateFonts_Version_1.0.1/generateFonts/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace generateFonts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fontComboBox = new System.Windows.Forms.ComboBox();
+            this.fontSizeComboBox = new System.Windows.Forms.ComboBox();
+            this.fontStyleComboBox = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nerwToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.exctendedCharsCheckBox = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fontComboBox
+            // 
+            this.fontComboBox.BackColor = System.Drawing.SystemColors.Window;
+            resources.ApplyResources(this.fontComboBox, "fontComboBox");
+            this.fontComboBox.FormattingEnabled = true;
+            this.fontComboBox.Name = "fontComboBox";
+            this.fontComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // fontSizeComboBox
+            // 
+            this.fontSizeComboBox.BackColor = System.Drawing.SystemColors.Window;
+            this.fontSizeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.fontSizeComboBox, "fontSizeComboBox");
+            this.fontSizeComboBox.FormattingEnabled = true;
+            this.fontSizeComboBox.Items.AddRange(new object[] {
+            resources.GetString("fontSizeComboBox.Items"),
+            resources.GetString("fontSizeComboBox.Items1"),
+            resources.GetString("fontSizeComboBox.Items2"),
+            resources.GetString("fontSizeComboBox.Items3"),
+            resources.GetString("fontSizeComboBox.Items4"),
+            resources.GetString("fontSizeComboBox.Items5"),
+            resources.GetString("fontSizeComboBox.Items6"),
+            resources.GetString("fontSizeComboBox.Items7"),
+            resources.GetString("fontSizeComboBox.Items8"),
+            resources.GetString("fontSizeComboBox.Items9"),
+            resources.GetString("fontSizeComboBox.Items10"),
+            resources.GetString("fontSizeComboBox.Items11"),
+            resources.GetString("fontSizeComboBox.Items12"),
+            resources.GetString("fontSizeComboBox.Items13"),
+            resources.GetString("fontSizeComboBox.Items14"),
+            resources.GetString("fontSizeComboBox.Items15"),
+            resources.GetString("fontSizeComboBox.Items16"),
+            resources.GetString("fontSizeComboBox.Items17"),
+            resources.GetString("fontSizeComboBox.Items18"),
+            resources.GetString("fontSizeComboBox.Items19"),
+            resources.GetString("fontSizeComboBox.Items20"),
+            resources.GetString("fontSizeComboBox.Items21"),
+            resources.GetString("fontSizeComboBox.Items22"),
+            resources.GetString("fontSizeComboBox.Items23"),
+            resources.GetString("fontSizeComboBox.Items24"),
+            resources.GetString("fontSizeComboBox.Items25"),
+            resources.GetString("fontSizeComboBox.Items26"),
+            resources.GetString("fontSizeComboBox.Items27"),
+            resources.GetString("fontSizeComboBox.Items28"),
+            resources.GetString("fontSizeComboBox.Items29"),
+            resources.GetString("fontSizeComboBox.Items30"),
+            resources.GetString("fontSizeComboBox.Items31"),
+            resources.GetString("fontSizeComboBox.Items32"),
+            resources.GetString("fontSizeComboBox.Items33"),
+            resources.GetString("fontSizeComboBox.Items34"),
+            resources.GetString("fontSizeComboBox.Items35"),
+            resources.GetString("fontSizeComboBox.Items36"),
+            resources.GetString("fontSizeComboBox.Items37"),
+            resources.GetString("fontSizeComboBox.Items38"),
+            resources.GetString("fontSizeComboBox.Items39"),
+            resources.GetString("fontSizeComboBox.Items40"),
+            resources.GetString("fontSizeComboBox.Items41"),
+            resources.GetString("fontSizeComboBox.Items42"),
+            resources.GetString("fontSizeComboBox.Items43"),
+            resources.GetString("fontSizeComboBox.Items44"),
+            resources.GetString("fontSizeComboBox.Items45"),
+            resources.GetString("fontSizeComboBox.Items46"),
+            resources.GetString("fontSizeComboBox.Items47"),
+            resources.GetString("fontSizeComboBox.Items48"),
+            resources.GetString("fontSizeComboBox.Items49"),
+            resources.GetString("fontSizeComboBox.Items50"),
+            resources.GetString("fontSizeComboBox.Items51"),
+            resources.GetString("fontSizeComboBox.Items52"),
+            resources.GetString("fontSizeComboBox.Items53"),
+            resources.GetString("fontSizeComboBox.Items54"),
+            resources.GetString("fontSizeComboBox.Items55"),
+            resources.GetString("fontSizeComboBox.Items56"),
+            resources.GetString("fontSizeComboBox.Items57"),
+            resources.GetString("fontSizeComboBox.Items58"),
+            resources.GetString("fontSizeComboBox.Items59"),
+            resources.GetString("fontSizeComboBox.Items60"),
+            resources.GetString("fontSizeComboBox.Items61"),
+            resources.GetString("fontSizeComboBox.Items62"),
+            resources.GetString("fontSizeComboBox.Items63"),
+            resources.GetString("fontSizeComboBox.Items64"),
+            resources.GetString("fontSizeComboBox.Items65"),
+            resources.GetString("fontSizeComboBox.Items66"),
+            resources.GetString("fontSizeComboBox.Items67"),
+            resources.GetString("fontSizeComboBox.Items68"),
+            resources.GetString("fontSizeComboBox.Items69"),
+            resources.GetString("fontSizeComboBox.Items70"),
+            resources.GetString("fontSizeComboBox.Items71"),
+            resources.GetString("fontSizeComboBox.Items72"),
+            resources.GetString("fontSizeComboBox.Items73"),
+            resources.GetString("fontSizeComboBox.Items74"),
+            resources.GetString("fontSizeComboBox.Items75"),
+            resources.GetString("fontSizeComboBox.Items76"),
+            resources.GetString("fontSizeComboBox.Items77"),
+            resources.GetString("fontSizeComboBox.Items78"),
+            resources.GetString("fontSizeComboBox.Items79"),
+            resources.GetString("fontSizeComboBox.Items80"),
+            resources.GetString("fontSizeComboBox.Items81"),
+            resources.GetString("fontSizeComboBox.Items82"),
+            resources.GetString("fontSizeComboBox.Items83"),
+            resources.GetString("fontSizeComboBox.Items84"),
+            resources.GetString("fontSizeComboBox.Items85"),
+            resources.GetString("fontSizeComboBox.Items86"),
+            resources.GetString("fontSizeComboBox.Items87"),
+            resources.GetString("fontSizeComboBox.Items88"),
+            resources.GetString("fontSizeComboBox.Items89"),
+            resources.GetString("fontSizeComboBox.Items90"),
+            resources.GetString("fontSizeComboBox.Items91")});
+            this.fontSizeComboBox.Name = "fontSizeComboBox";
+            this.fontSizeComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // fontStyleComboBox
+            // 
+            this.fontStyleComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.fontStyleComboBox, "fontStyleComboBox");
+            this.fontStyleComboBox.FormattingEnabled = true;
+            this.fontStyleComboBox.Items.AddRange(new object[] {
+            resources.GetString("fontStyleComboBox.Items"),
+            resources.GetString("fontStyleComboBox.Items1"),
+            resources.GetString("fontStyleComboBox.Items2")});
+            this.fontStyleComboBox.Name = "fontStyleComboBox";
+            this.fontStyleComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nerwToolStripMenuItem,
+            this.loadFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            resources.ApplyResources(this.menuToolStripMenuItem, "menuToolStripMenuItem");
+            // 
+            // nerwToolStripMenuItem
+            // 
+            this.nerwToolStripMenuItem.Name = "nerwToolStripMenuItem";
+            resources.ApplyResources(this.nerwToolStripMenuItem, "nerwToolStripMenuItem");
+            this.nerwToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // loadFileToolStripMenuItem
+            // 
+            this.loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            resources.ApplyResources(this.loadFileToolStripMenuItem, "loadFileToolStripMenuItem");
+            this.loadFileToolStripMenuItem.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // exctendedCharsCheckBox
+            // 
+            resources.ApplyResources(this.exctendedCharsCheckBox, "exctendedCharsCheckBox");
+            this.exctendedCharsCheckBox.Name = "exctendedCharsCheckBox";
+            this.exctendedCharsCheckBox.UseVisualStyleBackColor = true;
+            this.exctendedCharsCheckBox.CheckedChanged += new System.EventHandler(this.exctendedCharsCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.exctendedCharsCheckBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.fontStyleComboBox);
+            this.Controls.Add(this.fontSizeComboBox);
+            this.Controls.Add(this.fontComboBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox fontComboBox;
+        private System.Windows.Forms.ComboBox fontSizeComboBox;
+        private System.Windows.Forms.ComboBox fontStyleComboBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nerwToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox exctendedCharsCheckBox;
+    }
+}
+
---
+++ Code/Beta_test/generateFonts_Version_1.0.1/generateFonts/preview.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace generateFonts
+{
+    public partial class preview : Form
+    {
+        List<List<string>> m_glyphs = new List<List<string>>();
+        List<string> m_glyphData = new List<string>();
+        string m_fileName = "";
+        editor editBox = null;
+        bool allowClose = false;
+
+        public preview()
+        {
+            InitializeComponent();
+            editBox = new editor(this);
+        }
+
+
+        private void preview_Load(object sender, EventArgs e)
+        {
+            ((ToolStripDropDownMenu)optionsToolStripMenuItem.DropDown).ShowImageMargin = false;
+            ((ToolStripDropDownMenu)optionsToolStripMenuItem.DropDown).ShowCheckMargin = false;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown || allowClose)
+            {
+                if (editBox != null)
+                {
+                    editBox.overrideClose();
+                    Action close = () => editBox.Close();
+                    if (InvokeRequired)
+                        Invoke(close);
+                    else
+                        close();
+                    editBox = null;
+                }
+
+                allowClose = false;
+                return;
+            }
+
+            e.Cancel = true;
+
+            switch (MessageBox.Show(this, "Are you sure you want to finish editing the font", "Finish Editing Font", MessageBoxButtons.YesNo))
+            {
+                case DialogResult.Yes:
+                    Hide();
+                    editBox.Hide();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        internal void setData(List<List<string>> a_glyphs, List<string> a_glyphData, string a_fileName)
+        {
+            m_glyphs = a_glyphs;
+            m_glyphData = a_glyphData;
+            m_fileName = a_fileName;
+            if (a_glyphs.Count > 0 && a_glyphData.Count > 0) // preload 
+                editBox.setData(a_glyphs[1], a_glyphData[1]);
+            loadFont();
+        }
+
+        private void loadFont()
+        {
+            int xPos = 10;
+            int yPos = 10;
+            foreach (List<string> subList in m_glyphs)
+            {
+                string[] glyphData = m_glyphData[m_glyphs.IndexOf(subList)].Split(',');
+
+                string charHex = glyphData[0];
+                int charByteCount = Convert.ToInt32(glyphData[1]);
+                int charWidth = Convert.ToInt32(glyphData[2]);
+                int charHeight = Convert.ToInt32(glyphData[3]);
+
+                //
+                Panel panel1 = new Panel();
+                Label label1 = new Label();
+                // Initialize the Label and TextBox controls.
+                label1.Name = "Label" + charHex;
+                label1.Size = new Size(33, 14);
+                label1.AutoSize = true;
+                label1.Text = charHex; 
+                int panelWidth = label1.Width > (charWidth + 1) ? label1.Width : (charWidth + 1);
+                label1.Location = new Point((panelWidth - label1.Width) / 2, 0);
+                label1.Cursor = Cursors.Hand;
+                label1.Click += delegate (object sender, EventArgs e) { button_Click(sender, e, label1.Name.Replace("Label", "")); };
+                // Initialize the Panel control.
+                panel1.Name = "Panel" + charHex;
+                panel1.Location = new Point(xPos, yPos);
+                panel1.Size = new Size(panelWidth, charHeight + 20);
+                panel1.BorderStyle = BorderStyle.Fixed3D;
+                panel1.Cursor = Cursors.Hand;
+                panel1.Click += delegate (object sender, EventArgs e) { button_Click(sender, e, panel1.Name.Replace("Panel", "")); };
+                // 
+                PictureBox pic = new PictureBox();
+                pic.Location = new Point((panelWidth - (charWidth + 1)) / 2, 14);
+                pic.Name = "Pic" + charHex;
+                pic.Size = new Size(charWidth + 1, charHeight + 1);
+                pic.BackColor = Color.White;
+                pic.Image = new Bitmap(charWidth + 1, charHeight + 1);
+                pic.Cursor = Cursors.Hand;
+                pic.Click += delegate (object sender, EventArgs e) { button_Click(sender, e, pic.Name.Replace("Pic", "")); };
+                xPos += charWidth + 10;
+
+                int widthCount = 0, heightCount = 0;
+
+                foreach (string hex in subList)
+                {
+                    byte data = Convert.ToByte(hex.Replace("\t",""), 16);
+
+                    for (int b = 7; b >= 0; b--)
+                    {
+                        if (GetBit(data, b) == true)
+                        {
+                            ((Bitmap)pic.Image).SetPixel(widthCount, heightCount, Color.Black);
+                        }
+
+                        if (widthCount < charWidth - 1)
+                            widthCount++;
+                        else
+                        {
+                            widthCount = 0;
+                            if (heightCount < charHeight)
+                                heightCount++;
+                        }
+                    }                       
+                }
+
+                flowLayoutPanel1.Controls.Add(panel1);
+                flowLayoutPanel1.Refresh();
+                panel1.Controls.Add(label1);
+                panel1.Controls.Add(pic);
+            }
+        }
+
+        public static bool GetBit(byte b, int bitNumber)
+        {
+            return (b & (1 << bitNumber)) != 0;
+        }
+
+        void button_Click(object sender, EventArgs e, string name)
+        {
+            int n = Convert.ToByte(name, 16) - 32;
+            string glyphData = m_glyphData[n];
+            List<string> glyphs = m_glyphs[n];
+            editBox.Show();
+            editBox.setData(glyphs, glyphData);         
+            Console.WriteLine("Picturebox " + name + " clicked");
+        }
+
+        internal void UpdateGlyph(List<string> a_glyphs, int a_arrayPos, int a_width, int a_height, int a_yOffset, bool autoCropped)
+        {
+            m_glyphs[a_arrayPos] = a_glyphs.ToList();
+
+            string id = "0x" + (32 + a_arrayPos).ToString("X2");
+            int widthCount = 0, heightCount = 0;
+            PictureBox pic = flowLayoutPanel1.Controls.Find("Pic" + id, true).FirstOrDefault() as PictureBox;
+            Label label = flowLayoutPanel1.Controls.Find("Label" + id, true).FirstOrDefault() as Label;
+            Panel panel = flowLayoutPanel1.Controls.Find("Panel" + id, true).FirstOrDefault() as Panel;
+
+            int panelWidth = label.Width > a_width ? label.Width : a_width;
+            label.Location = new Point((panelWidth - label.Width) / 2, 0);
+            panel.Size = new Size(panelWidth, a_height + 20);
+            pic.Size = new Size(a_width, a_height);
+            pic.Location = new Point((panelWidth - a_width) / 2, 14);
+            pic.Image = new Bitmap(a_width, a_height);
+
+            string fontInfo = id + "," + a_glyphs.Count + "," + a_width + "," + a_height + "," + a_yOffset;
+            m_glyphData[a_arrayPos] = fontInfo;           
+                
+            int totalCount = 0;
+
+            foreach (string hex in a_glyphs)
+            {
+                byte data = Convert.ToByte(hex, 16);
+
+                for (int b = 7; b >= 0; b--)
+                {
+                    if (totalCount < a_width * a_height)
+                    {
+                        if (GetBit(data, b) == true)
+                        {
+                            ((Bitmap)pic.Image).SetPixel(widthCount, heightCount, Color.Black);
+                        }
+                        else
+                        {
+                            ((Bitmap)pic.Image).SetPixel(widthCount, heightCount, Color.White);
+                        }
+                        if (widthCount < a_width - 1)
+                            widthCount++;
+                        else
+                        {
+                            widthCount = 0;
+                            heightCount++;
+                        }
+                        totalCount++;
+                    }
+                }
+
+            }
+            pic.Refresh();
+            flowLayoutPanel1.Refresh();          
+
+            // Update glyph array in file
+            List<string> lines = new List<string>();
+            List<string> fileData = new List<string>();
+
+            using (StreamReader reader = new StreamReader(m_fileName))
+            {              
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            bool continueInserting = false;
+            bool insertComplete = false;
+            bool foundMatch = false;
+            int insertCnt = 0;
+            string hexVal = "  ";
+           // string endLine = "";
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                if (lines[i].Contains("Auto Cropped:"))
+                {
+                    lines[i] = "// Auto Cropped: " + (autoCropped ? "true" : "false");
+                }
+                else if (lines[i].Contains("Font creation date:"))
+                {
+                    lines[i] = "// Font creation date: " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString();
+                }
+                if (!foundMatch && lines[i].Contains("_char_" + id + "["))
+                {
+                    fileData[i - 1] = "// Height: " + a_height + "px";
+                    fileData[i - 2] = "// Width: " + a_width + "px";
+                    string[] title = lines[i].Split('[');
+                    fileData.Add(title[0] + "[" + a_glyphs.Count() + "] PROGMEM = {");
+                    foundMatch = true;
+                }
+                else if (!foundMatch || continueInserting)
+                {
+                    fileData.Add(lines[i]);
+                }
+                else if (insertComplete && lines[i].Contains("};") && !continueInserting)
+                {
+                    string[] dataStruct = lines[i + 1].Split(',');
+                    fileData.Add("};");
+                    fileData.Add(dataStruct[0] + ", " + a_glyphs.Count + ", " + a_width + ", " + a_height + ", " + a_yOffset + " };");
+                    i++;
+                    continueInserting = true;
+                }
+                else if (foundMatch && !insertComplete)
+                {
+                    foreach (string str in a_glyphs)
+                    {
+                        insertCnt++;
+                        if (!str.Contains("0x"))
+                            hexVal += "0x" + str + ", ";
+                        else
+                            hexVal += str + ", ";                          
+                    }
+                    fileData.Add(hexVal);
+                    insertComplete = true;
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter(m_fileName))
+            {
+                foreach(string str in fileData)
+                {
+                    writer.WriteLine(str);
+                }
+            }
+
+            if (m_glyphs.Count >= a_arrayPos && m_glyphData.Count >= a_arrayPos) // preload 
+                editBox.setData(m_glyphs[a_arrayPos], m_glyphData[a_arrayPos]);
+        }
+
+        internal void overrideClose()
+        {
+            allowClose = true;
+        }
+
+        private void autoCropAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int n = 1; n < m_glyphData.Count(); n++)
+            {
+                string glyphData = m_glyphData[n];
+                List<string> glyphs = m_glyphs[n];
+                editBox.setData(glyphs, glyphData);
+                editBox.autoCropToolStripMenuItem_Click(sender, e);
+                editBox.saveToolStripMenuItem_Click(sender, e);
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            switch (MessageBox.Show(this, "Are you sure you want to finish editing the font", "Finish Editing Font", MessageBoxButtons.YesNo))
+            {
+                case DialogResult.Yes:
+                    Hide();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
